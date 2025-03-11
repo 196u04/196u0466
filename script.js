f07a67ca762cc1543830f528546868d3d1728760
196u04 <196u0466@gmail.com>
2025-03-11 14:58:00 +0800

    Create script.js
    輸入密碼 script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function checkInput() {
+    const userInput = document.getElementById("userInput").value;
+    const correctPassword = "1234";  // 這裡可以改成你要的密碼
+
+    if (userInput === correctPassword) {
+        window.location.href = "page2.html";  // 輸入正確時跳轉
+    } else {
+        document.getElementById("message").innerText = "密碼錯誤，請再試一次！";
+    }
+}
