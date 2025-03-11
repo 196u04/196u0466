a5045549a277133ed549e0a73073c6d9f2bc9bf2
196u04 <196u0466@gmail.com>
2025-03-11 21:30:15 +0800

    Update script.js
    add checkInput2

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function checkInput() {
         document.getElementById("message").innerText = "密碼錯誤，請再試一次！";
     }
 }
+function checkInput2() {
+    const userInput = document.getElementById("userInput").value.trim();  // 去掉頭尾空格
+    const correctAnswer = `cout>>Pokemongo;
+return 0;
+請正確輸入`;  // 正確答案（可換行）
+
+    if (userInput === correctAnswer) {
+        window.location.href = "page3.html";  // 跳轉到下一頁
+    } else {
+        document.getElementById("message").innerText = "答案錯誤，請再試一次！";
+    }
+}
